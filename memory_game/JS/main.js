f9525e101323ef90f2025faf81b6b3a4f7203a23
lelandshir <lelandshir@gmail.com>
2020-04-10 19:27:37 -0700

    created game logic

---
+++ memory_game/JS/main.js
@@ -0,0 +1,9 @@
+console.log("Up and running!");
+
+let cardOne = "King";
+let cardTwo = "King";
+let cardThree = "Queen";
+let cardFour = "Queen";
+
+console.log("User flipped " + cardThree);
+console.log("User flipped " + cardOne);
