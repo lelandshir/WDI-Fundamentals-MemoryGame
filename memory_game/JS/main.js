3eaecba1da188a1bcfe10011b39fc7ff567eb3e1
lelandshir <lelandshir@gmail.com>
2020-04-14 00:00:36 -0700

    created game logic

--- memory_game/JS/main.js
+++ memory_game/JS/main.js
@@ -1,9 +1,22 @@
-console.log("Up and running!");
-
-let cardOne = "King";
-let cardTwo = "King";
-let cardThree = "Queen";
-let cardFour = "Queen";
-
-console.log("User flipped " + cardThree);
-console.log("User flipped " + cardOne);
+//arrays
+const cards = ["queen", "queen", "king", "king"];
+const cardsInPlay = [];
+//variables for alerts below
+const noMatch = "Sorry, try again.";
+const match = "You found a match!";
+//variables for cards below
+const cardOne = cards[0];
+const cardTwo = cards[2];
+//push
+cardsInPlay.push(cardOne, cardTwo);
+//console.logs
+console.log("User flipped a " + cardOne);
+console.log("User flipped a " + cardTwo);
+//if else statement
+if(cardsInPlay.length === 2) {
+	console.log("true");
+} if(cards[0] === cards[0]) {
+	alert(match);
+} else {
+	alert(noMatch);
+}
