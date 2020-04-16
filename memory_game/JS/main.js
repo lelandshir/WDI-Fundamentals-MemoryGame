4def6d7929dcc4a1bafe4a287c103e44c115803f
lelandshir <lelandshir@gmail.com>
2020-04-16 05:17:06 -0700

    created game logic

--- memory_game/JS/main.js
+++ memory_game/JS/main.js
@@ -1,60 +1,46 @@
-const cards = ["queen", "queen", "king", "king"];
+const cards = [
+{
+	rank: "Queen",
+	suit: "hearts",
+	cardImage: "images/queen-of-hearts.png"
+
+},
+{
+	rank: "Queen",
+	suit: "diamonds",
+	cardImage: "images/queen-of-diamonds.png"
+},
+{
+	rank: "King",
+	suit: "hearts",
+	cardImage: "images/king-of-hearts.png"
+},
+{
+rank: "King",
+	suit: "diamonds",
+	cardImage: "images/king-of-diamonds.png"
+}
+];
+
 const cardsInPlay = [];
 
 function checkForMatch() {
 	if (cardsInPlay[0] === cardsInPlay[1]) {
-	console.log("Great, you found a match.");
+	alert("Great, you found a match.");
 } else {
-	console.log("Sorry, please try again.");
-}
+	alert("Sorry, please try again.");
+	}
 };
 
 function flipCard(cardId) {
-	checkForMatch();
-	console.log("User flipped " + cards[cardId]);
-	cardsInPlay.push(cardId);
+	cardsInPlay.push(cards[cardId].rank);
+	if (cardsInPlay.length === 2)
+		checkForMatch();
+	console.log("User flipped " + cards[cardId].rank);
+	console.log(cards[cardId].cardImage);
+	console.log(cards[cardId].suit);
+	
 };
 
 flipCard(0);
 flipCard(2);
-
-
-
-
-
-
-//Memory Card Game Logic 
-//User flips a card a card, is told what card they flipped --> "User flipped queen (or king)"
-//The card that the user has flipped gets added to the cardsInPlay array
-//User flips a second card, is told what card they flipped once again---"User flipped king (or queen)"
-//The csecond card that the user flipped gets added to the cardInPlay array once again
-//When the cardInPlay array length is equal to 2, we check to see if those two cards are equal
-//if those cards are equal, "You found a match!"
-//otherwise, "Sorry Try Again!"
-
-
-
-
-
-
-//  function checkForMatch() {
-//   //if (cardsInPlay.length == 2) {
-//     if (cardsInPlay[0] === cardsInPlay[1]) {
-//           alert("You found a match!");
- 
-//       } else {
-//           alert("Sorry, try again");
-//         };
-
-//   };
-// function flipCard(cardId) {
-// 	console.log("User flipped " + cards[cardId]);
-// 	cardsInPlay.push(cards[cardId]);
-	
-
-// if(cardsInPlay.length === 2 && cards[0] === cards[2]) {
-// 	alert("You Found A Match!");
-// } else {
-// 	alert("Sorry, try again!");
-// } return;
-// };
